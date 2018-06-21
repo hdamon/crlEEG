d6e30b7e20c74640f1927792c45f0c4916fedd4c
Damon Hyde <damon.hyde@childrens.harvard.edu>
2018-06-21 17:28:28 -0400

    Removed unnecessary functions

--- +crlEEG/+inDev/testLabelledArray.m
+++
@@ -1,4 +0,0 @@
-%%
-clear all; clear classes;
-for i = 1:100, labels{i} = ['Chan' num2str(i)]; end;
-test = labelledArray_withValues(randn(10000,100,3),'dimLabels',{2,labels},'dimValues',{1,1:10000; 2,linspace(0,1,100)},'dimNames',{'A' 'B' 'C'});
