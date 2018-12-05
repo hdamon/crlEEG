784d222b839c366f977e4d642b38cebfc172b503
D. Hyde <damon.hyde@philips.com>
2018-12-05 09:39:57 -0500

    Simplifies crlEEG.sensor.electrode subreferencing.

--- +crlEEG/+sensor/@electrode/numArgumentsFromSubscript.m
+++ +crlEEG/+sensor/@electrode/numArgumentsFromSubscript.m
@@ -1,3 +1,9 @@
-function n = numArgumentsFromSubscript(obj,~,indexingContext)    
-  n = 1; return;  
+function n = numArgumentsFromSubscript(obj,s,indexingContext)    
+
+   if ismember(s(end).subs,{'plot3D','plot2D','center','basis','projPos'})
+     n = 1;
+   else
+     n = builtin('numArgumentsFromSubscript',obj,s,indexingContext);
+   end
+   
 end
--- +crlEEG/+sensor/@electrode/subsref.m
+++ +crlEEG/+sensor/@electrode/subsref.m
@@ -1,96 +1,106 @@
 function varargout = subsref(obj,s)
-switch s(1).type
-  case '.'
-    if length(s) == 1
-      % Implement obj.PropertyName      
-      switch s(1).subs
-        case {'plot3D','plot2D','center','basis','projPos'}
-          % This is really a bit of a hack, and something should be done to
-          % improve this behavior.
-          %obj.(s(1).subs);
-          varargout = {obj.(s(1).subs)};
-          return;
-        
-        case {'center'}
-          % More hack code
-          varargout = {obj.center};
-                          
-        case 'basis'
-          % More hack code
-          varargout = {obj.basis};
-          
-        otherwise          
-          tmp = cell(size(obj));
-          for i = 1:numel(obj)
-            tmp{i} = builtin('subsref',obj(i),s);
-          end;
-          
-          switch s(1).subs
-            case 'position'
-              % This returns an array
-              varargout = {cat(1,tmp{:})};
-                         
-            otherwise
-              % Everything else returns a cell array
-              varargout = {tmp};
-          end;          
-      end;
-      %varargout = {builtin('subsref',obj,s)};
-      
-    elseif length(s) == 2 && strcmp(s(2).type,'()')
-      % Implement obj.PropertyName(indices)
-      switch s(1).subs
-        case {'plot2D', 'plot3D', 'getNumericIndex','projPos'}
-          % This if for calling these with additional arguments
-          %obj.(s(1).subs)(s(2).subs{:});
-          varargout = {obj.(s(1).subs)(s(2).subs{:})};
-        otherwise
-          tmp = obj.subsref(s(1));
-          varargout = {builtin('subsref',tmp,s(2))};
-      end;
-      
-    else
-      varargout = {builtin('subsref',obj,s)};
-    end
-    
-  case '()'
-    if length(s) == 1
-      % Implement obj(indices)
-      varargout = {indexObj(obj,s)};     
-      
-    elseif length(s) == 2 && strcmp(s(2).type,'.')
-      % Implement obj(ind).PropertyName
-      [tmpObj,remS] = indexObj(obj,s);
-      varargout = {tmpObj.subsref(remS)};      
-      
-    elseif length(s) == 3 && strcmp(s(2).type,'.') && strcmp(s(3).type,'()')
-      % Implement obj(indices).PropertyName(indices)      
-      [tmpObj,remS] = indexObj(obj,s);
-      varargout = {tmpObj.subsref(remS)};
-      
-    else
-      % Use built-in for any other expression
-      [tmpObj,remS] = indexObj(obj,s);
-      varargout = {tmpObj.subsref(remS)};
-    end
-    
-  case '{}'
-    error('Brace indexing not supported by crlEEG.type.sensor.electrode');
-    if length(s) == 1
-      % Implement obj{indices}
-      varargout = {builtin('subsref',obj,s)};
-    elseif length(s) == 2 && strcmp(s(2).type,'.')
-      % Implement obj{indices}.PropertyName
-      varargout = {builtin('subsref',obj,s)};
-    else
-      % Use built-in for any other expression
-      varargout = {builtin('subsref',obj,s)};
-    end
-  otherwise
-    error('Not a valid indexing expression')
+
+% Shift name based indexing to numeric.
+for i = 1:numel(s)
+  if isequal(s(i).type,'()')
+    s(i).subs = {obj.getNumericIndex(s(i).subs{:})};
+  end
 end
 
-
+[varargout{1:nargout}] = builtin('subsref',obj,s);
+% 
+% switch s(1).type
+%   case '.'
+%      [varargout{1:nargout}] = builtin('subsref',obj,s);
+%      
+%     if length(s) == 1
+%       % Implement obj.PropertyName      
+%       switch s(1).subs
+%         case {'plot3D','plot2D','center','basis','projPos'}
+%           % This is really a bit of a hack, and something should be done to
+%           % improve this behavior.
+%           %obj.(s(1).subs);
+%           varargout = {obj.(s(1).subs)};
+%           return;
+%         
+%         case {'center'}
+%           % More hack code
+%           varargout = {obj.center};
+%                           
+%         case 'basis'
+%           % More hack code
+%           varargout = {obj.basis};
+%           
+%         otherwise          
+%           tmp = cell(size(obj));
+%           for i = 1:numel(obj)
+%             tmp{i} = builtin('subsref',obj(i),s);
+%           end
+%           
+%           switch s(1).subs
+%             case 'position'
+%               % This returns an array
+%               varargout = {cat(1,tmp{:})};
+%                          
+%             otherwise
+%               % Everything else returns a cell array
+%               varargout = {tmp};
+%           end         
+%       end
+%       %varargout = {builtin('subsref',obj,s)};
+%       
+%     elseif length(s) == 2 && strcmp(s(2).type,'()')
+%       % Implement obj.PropertyName(indices)
+%       switch s(1).subs
+%         case {'plot2D', 'plot3D', 'getNumericIndex','projPos'}
+%           % This if for calling these with additional arguments
+%           %obj.(s(1).subs)(s(2).subs{:});
+%           varargout = {obj.(s(1).subs)(s(2).subs{:})};
+%         otherwise
+%           tmp = obj.subsref(s(1));
+%           varargout = {builtin('subsref',tmp,s(2))};
+%       end
+%       
+%     else
+%       varargout = {builtin('subsref',obj,s)};
+%     end
+%     
+%   case '()'
+%     if length(s) == 1
+%       % Implement obj(indices)
+%       varargout = {indexObj(obj,s)};     
+%       
+%     elseif length(s) == 2 && strcmp(s(2).type,'.')
+%       % Implement obj(ind).PropertyName
+%       [tmpObj,remS] = indexObj(obj,s);
+%       varargout = {tmpObj.subsref(remS)};      
+%       
+%     elseif length(s) == 3 && strcmp(s(2).type,'.') && strcmp(s(3).type,'()')
+%       % Implement obj(indices).PropertyName(indices)      
+%       [tmpObj,remS] = indexObj(obj,s);
+%       varargout = {tmpObj.subsref(remS)};
+%       
+%     else
+%       % Use built-in for any other expression
+%       [tmpObj,remS] = indexObj(obj,s);
+%       varargout = {tmpObj.subsref(remS)};
+%     end
+%     
+%   case '{}'
+%     error('Brace indexing not supported by crlEEG.type.sensor.electrode');
+%     if length(s) == 1
+%       % Implement obj{indices}
+%       varargout = {builtin('subsref',obj,s)};
+%     elseif length(s) == 2 && strcmp(s(2).type,'.')
+%       % Implement obj{indices}.PropertyName
+%       varargout = {builtin('subsref',obj,s)};
+%     else
+%       % Use built-in for any other expression
+%       varargout = {builtin('subsref',obj,s)};
+%     end
+%   otherwise
+%     error('Not a valid indexing expression')
+% end
 
 end
 
@@ -104,10 +114,10 @@ if iscellstr(s(1).subs{1})||ischar(s(1).subs{1})
    objOut = obj(outIdx);
   else
     error('Invalid index');
-  end;
+  end
 else
   objOut = builtin('subsref',obj,s(1));
-end;
+end
 
 remS = s(2:end);
 end
