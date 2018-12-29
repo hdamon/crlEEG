9d968856cc0b633d33c75ce7333702175474c4fc
D. Hyde <damon.hyde@philips.com>
2018-12-29 12:24:37 -0500

    Updates dataTopo to current GUI standards.

--- +crlEEG/+inDev/dataTopo.m
+++ +crlEEG/+inDev/dataTopo.m
@@ -14,41 +14,54 @@ function varargout = dataTopo(elec,data,varargin)
 % 2009-2018
 %
 
-figure;
 
+% Input Parsing
 p = inputParser;
 p.KeepUnmatched = true;
 p.addRequired('elec');
 p.addRequired('data');
-p.addParameter('cmap',[],@(x) isa(x,'crlEEG.gui.widget.alphacolor'));
+p.addParameter('cmap',[],@(x) isa(x,'guiTools.widget.alphacolor'));
+p.addParameter('figure',[],@(x) ishghandle(x));
 p.addParameter('axis',[],@(x) ishghandle(x));
 p.parse(elec,data,varargin{:});
+
+% Figure Selection
+if ~isempty(p.Results.figure)
+  figure(p.Results.figure);
+else
+  figure;
+end
 
 % Set Default Colormap
 cmap = p.Results.cmap;
 if isempty(cmap)
-  cmap = crlEEG.gui.widget.alphacolor; 
+  cmap = guiTools.widget.alphacolor; 
   cmap.range = [min(data(:)) max(data(:))];
-end;
+end
 
+% Plot Electrode Locations
 elecPlot = elec.plot2D('figure',gcf,varargin{:});
+elecPlot = elecPlot(1);
+
+% Draw Topographic Plot
 topoPlot = drawTopo(data,elecPlot.scatter.XData,...
                          elecPlot.scatter.YData,...
                          'cmap',cmap,varargin{:});
 uistack(elecPlot.scatter,'top');
 if isfield(elecPlot,'text')
   uistack(elecPlot.text,'top');
-end;
+end
 
-topoOut.figure = gcf;
-topoOut.axis = gca;
-topoOut.elecPlot = elecPlot;
-topoOut.topoPlot = topoPlot;
-topoOut.headCartoon = crlEEG.gui.util.drawHeadCartoon(gca);
+  topoOut.figure = gcf;
+  topoOut.axis = gca;
+  topoOut.elecPlot = elecPlot;
+  topoOut.topoPlot = topoPlot;
+  topoOut.headCartoon = guiTools.util.drawHeadCartoon(gca);
 
+% Optional Outputs
 if nargout>0
   varargout{1} = topoOut;
-end;
+end
 
 end
 
@@ -65,23 +78,24 @@ function varargout = drawTopo(data,x,y,varargin)
   %  'plotDiam'  : Overall plotDiameter of plot 
   %  'scale' : Scaling of x-y Locations
   %  'circRadius' : Radius circle to display
-  %  '
+  %  'cmap' : guiTools.widget.alphacolor object
+  %
 
   %% Input Parsing
   p = inputParser;
   p.KeepUnmatched = true;
-  p.addParamValue('plotDiam',4);
-  p.addParamValue('xyScale',1);
-  p.addParamValue('circRadius',1.6);
-  p.addParamValue('cmap',[]);
+  p.addParameter('plotDiam',4);
+  p.addParameter('xyScale',1);
+  p.addParameter('circRadius',1.75);
+  p.addParameter('cmap',[],@(x) isa(x,'guiTools.widget.alphacolor'));
   p.parse(varargin{:});
  
   if isempty(p.Results.cmap)
-    cmap = crlEEG.gui.widget.alphacolor;
+    cmap = guiTools.widget.alphacolor;
     cmap.range = [min(data(:)) max(data(:))];
   else
     cmap = p.Results.cmap; 
-  end;
+  end
   topoOut.cmap = cmap;
   
   plotDiam = p.Results.plotDiam;
@@ -90,11 +104,11 @@ function varargout = drawTopo(data,x,y,varargin)
   %% Scale locations and interpolate on a regular grid
   x = scale*x; y = scale*y;
   F = scatteredInterpolant(x(:),y(:),data(:));
-  %F.ExtrapolationMethod = 'none';
+  F.ExtrapolationMethod = 'none';
   
   xGrid = linspace(-plotDiam/2,plotDiam/2,500);
   yGrid = linspace(-plotDiam/2,plotDiam/2,500);
-  [X Y] = meshgrid(xGrid,yGrid);
+  [X, Y] = meshgrid(xGrid,yGrid);
   
   D = F(X,Y);
   
@@ -111,11 +125,11 @@ function varargout = drawTopo(data,x,y,varargin)
   topoOut.img = image(xGrid,yGrid,Dimg);
   topoOut.img.AlphaData = ones(size(D));
   topoOut.img.AlphaData(isnan(D)) = 0;
-  if ~holdStatus, hold off; end;
+  if ~holdStatus, hold off; end
   
   a = gca; a.YDir = 'normal'; axis off;
   
   if nargout>0
     varargout{1} = topoOut;
-  end;
+  end
 end
